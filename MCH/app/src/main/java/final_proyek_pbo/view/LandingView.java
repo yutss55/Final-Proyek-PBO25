304e55e3f03e07fad97db012bfa3de40b0fb3bf1
fatichmaze <andifatimahazzahrah02@gmail.com>
2026-05-23 20:49:00 +0800

    feat: menambahkan UI landing page PINISI

--- MCH/app/src/main/java/final_proyek_pbo/view/LandingView.java
+++ MCH/app/src/main/java/final_proyek_pbo/view/LandingView.java
@@ -1,12 +1,9 @@
 package final_proyek_pbo.view;
 
-import java.beans.VetoableChangeListenerProxy;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.scene.Parent;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
-import javafx.scene.text.Text;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
@@ -14,6 +11,8 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 public class LandingView {
     private BorderPane root;
@@ -22,17 +21,33 @@ public class LandingView {
         root = new BorderPane();
         Label apk = new Label("PINISI");
         // root.setCenter(apk);
-        apk.setFont(Font.font("Inter", FontWeight.BOLD, 30));
+        apk.setFont(Font.font("Inter", FontWeight.BOLD, 36));
+                    apk.setStyle(
+                "-fx-font-size: 30px;" +
+                "-fx-font-weight: bold;" +
+                "-fx-text-fill: white;"
+            );
 
         HBox navbar = new HBox();
         navbar.setSpacing(20);
         navbar.setAlignment(Pos.CENTER_LEFT);
 
         Button btnhome = new Button("Home");
+        btnhome.setStyle("-fx-background-color: transparent;-fx-text-fill: white;");
         Button btncommunity = new Button("Community");
+        btncommunity.setStyle("-fx-background-color: transparent;-fx-text-fill: white;");
         Button btnEvent = new Button("Event");
+        btnEvent.setStyle("-fx-background-color: transparent; -fx-text-fill: white;");
         Button btnLogin = new Button("Login");
-        
+        btnLogin.setStyle(
+            "-fx-background-color: transparent;" +
+            "-fx-text-fill: white;" +
+            "-fx-border-color: white;" +
+            "-fx-border-radius: 20;" +
+            "-fx-background-radius: 20;" +
+            "-fx-padding: 1 10 1 10;"
+        );
+                
 
         Region spacer = new Region();
         HBox.setHgrow(spacer, Priority.ALWAYS);
@@ -56,22 +71,39 @@ public class LandingView {
 
         Label title = new Label("Pintar Ningkatin\nSkill IT");
         title.setStyle( "-fx-font-size: 40px;" +
-                        "-fx-font-weight: bold;");
+                        "-fx-font-weight: bold;"+ 
+                        "-fx-text-fill: white");
         Label text = new Label("Platform Pengembangan Talenta Digital\nMasyarakat Kota Makassar");
-        text.setStyle("-fx-font-size: 15px;");
+        text.setStyle("-fx-font-size: 15px;"+ "-fx-text-fill: white");
         Button btnStart = new Button("Get Started");
         btnStart.setStyle(
-            "-fx-background-radius: 20;" +
-            "-fx-padding: 10 20 10 20;"
+            "-fx-background-color: #6D4AFF;" +
+            "-fx-border-color: white;"+
+            "-fx-text-fill: white;" +
+            "-fx-font-size: 15px;" +
+            "-fx-font-weight: bold;" +
+            "-fx-background-radius: 25;" +
+            "-fx-border-radius: 40;"+
+            "-fx-padding: 12 25 12 25;"
         );
 
         leftcontent.getChildren().addAll(title, text, btnStart);
         leftcontent.setSpacing(20);
 
-        Label mascot = new Label("mascot");
+        Image image = new Image(
+                getClass().getResourceAsStream("/images/maskot.png")
+        );
+
+        ImageView mascot = new ImageView(image);
+
+        mascot.setFitWidth(350);
+        mascot.setPreserveRatio(true);
         
         pict.getChildren().addAll(leftcontent, mascot);
         root.setCenter(pict);
+        root.setStyle(
+             "-fx-background-color: linear-gradient(to right, #000033, #0600AB);"
+        );
     }
 
     public BorderPane getView() {
