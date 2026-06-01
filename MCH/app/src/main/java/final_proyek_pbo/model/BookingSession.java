7a4dacbf5a65eb7bb92e0d9a097d155f9667ac56
yutss55 <ayuanggraini210607@gmail.com>
2026-06-01 12:48:36 +0800

    Fix:file halaman komunitas

---
+++ MCH/app/src/main/java/final_proyek_pbo/model/BookingSession.java
@@ -0,0 +1,82 @@
+package final_proyek_pbo.model;
+
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+public class BookingSession {
+    
+    private final StringProperty idBooking;
+    private final StringProperty namaPeminjam;
+    private final StringProperty namaRuangan;
+    private final StringProperty tanggalSewa;
+    private final StringProperty jamSpesifik;
+    private final StringProperty keperluan;
+    private final StringProperty statusApproval;
+
+    public BookingSession(String idBooking, String namaPeminjam, String namaRuangan, 
+                          String tanggalSewa, String jamSpesifik, String keperluan, String statusApproval) {
+        this.idBooking = new SimpleStringProperty(idBooking);
+        this.namaPeminjam = new SimpleStringProperty(namaPeminjam);
+        this.namaRuangan = new SimpleStringProperty(namaRuangan);
+        this.tanggalSewa = new SimpleStringProperty(tanggalSewa);
+        this.jamSpesifik = new SimpleStringProperty(jamSpesifik);
+        this.keperluan = new SimpleStringProperty(keperluan);
+        this.statusApproval = new SimpleStringProperty(statusApproval);
+    }
+
+    public String getIdBooking() { 
+        return idBooking.get(); 
+    }
+
+    public String getNamaPeminjam() { 
+        return namaPeminjam.get(); 
+    }
+
+    public String getNamaRuangan() { 
+        return namaRuangan.get(); 
+    }
+
+    public String getTanggalSewa() { 
+        return tanggalSewa.get(); 
+    }
+
+    public String getJamSpesifik() { 
+        return jamSpesifik.get(); 
+    }
+
+    public String getKeperluan() { 
+        return keperluan.get(); 
+    }
+
+    public String getStatusApproval() { 
+        return statusApproval.get(); 
+    }
+
+    public StringProperty idBookingProperty() { 
+        return idBooking; 
+    }
+
+    public StringProperty namaPeminjamProperty() { 
+        return namaPeminjam; 
+    }
+
+    public StringProperty namaRuanganProperty() { 
+        return namaRuangan; 
+    }
+
+    public StringProperty tanggalSewaProperty() { 
+        return tanggalSewa; 
+    }
+
+    public StringProperty jamSpesifikProperty() { 
+        return jamSpesifik; 
+    }
+
+    public StringProperty keperluanProperty() { 
+        return keperluan; 
+    }
+
+    public StringProperty statusApprovalProperty() { 
+        return statusApproval; 
+    }
+}
