7349f60201b8d585ff7ca1bdc1be089ea099f17a
mukhsin27 <mukhsin230@gmail.com>
2026-06-01 14:20:51 +0800

    fix: memperbaiki navigasi halaman roadmap dari homeview

--- MCH/app/src/main/java/final_proyek_pbo/model/JalurKarir.java
+++ MCH/app/src/main/java/final_proyek_pbo/model/JalurKarir.java
@@ -11,16 +11,8 @@ public class JalurKarir {
         this.namaKarir = namaKarir;
         this.daftarTahapan = new ArrayList<>();
     }
-
-    public void tambahTahapan(Tahapan tahapan) {
-        this.daftarTahapan.add(tahapan);
-    }
-
+    public void tambahTahapan(Tahapan tahapan) { this.daftarTahapan.add(tahapan); }
     public String getNamaKarir() { return namaKarir; }
     public List<Tahapan> getDaftarTahapan() { return daftarTahapan; }
-
-    @Override
-    public String toString() {
-        return namaKarir;
-    }
+    @Override public String toString() { return namaKarir; }
 }
